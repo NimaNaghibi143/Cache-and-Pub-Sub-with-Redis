5c93381d1c99867a64748e8e4d5e4278c35a3d09
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-06-29 14:21:41 +0330

    feat: define the client to connect with the redis

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/redis/go-redis/v9"
+)
+
+func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	fmt.Println(client)
 }
